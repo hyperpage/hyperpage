77dcf402a287bc8a79f0dce95d4de18ce2a7b497
Damien Grauet <damien.grauet@gmail.com>
2025-10-26 00:25:47 +0200

    fix: restore CSS module type declarations to next-env.d.ts
    - Restored 'declare module *.css' declaration that was removed
    - Fixes TypeScript errors with CSS imports in Next.js components
    - Resolves 'Cannot find module' errors for CSS module imports

--- next-env.d.ts
+++ next-env.d.ts
@@ -4,6 +4,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-
-// CSS Module Declarations
-declare module '*.css';
